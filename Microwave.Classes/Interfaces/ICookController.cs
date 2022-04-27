fe49bf619d0332039228de3d3e4c83723605c53e
odennis2 <oscard@live.dk>
2022-04-27 15:02:02 +0200

    Comments added to ICookController

--- Microwave.Classes/Interfaces/ICookController.cs
+++ Microwave.Classes/Interfaces/ICookController.cs
@@ -10,6 +10,6 @@ namespace Microwave.Classes.Interfaces
     {
         void StartCooking(int power, int time);
         void Stop();
-        void TimeButton();
+        //void TimeButton();
     }
 }
